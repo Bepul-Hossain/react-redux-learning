03cda97c168e0d89a923775dd3952604490e95bf
Bepul-Hossain <bepul.cse.ru@gmail.com>
2021-05-08 14:26:45 +0600

    initial commit

--- src/app/common/util/reducerUtils.js
+++
@@ -1,7 +0,0 @@
-export const createReducer = (initalState, fnMap) => {
-  return (state = initalState, { type, payload }) => {
-    const handler = fnMap[type];
-
-    return handler ? handler(state, payload) : state;
-  };
-};
--- src/index.js
+++ src/index.js
@@ -1,38 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./app/layout/App";
-import * as serviceWorker from "./serviceWorker";
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import { configureStore } from "./app/store/configureStore";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-const store= configureStore();
-//console.log(store.getState());
-console.log(store.getState());
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-const rootEl = document.getElementById("root");
-let render = () => {
-  ReactDOM.render(
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>,
-
-    rootEl
-  );
-};
-if (module.hot) {
-  module.hot.accept("./app/layout/App", () => {
-    setTimeout(render);
-  });
-}
-render();
-
-//ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
