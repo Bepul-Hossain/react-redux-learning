3163887ebb9a70510e229e879fa7a603b5141294
Bepul-Hossain <bepul.cse.ru@gmail.com>
2021-05-10 11:47:57 +0600

    little advance action in redux

--- src/components/control.js
+++ src/components/control.js
@@ -1,18 +1,13 @@
 import React from "react";
 import { useDispatch } from "react-redux";
+import { add, sub } from "../store/actions/action";
 
 const Control = () => {
   const dispatch = useDispatch();
-  const add = () => {
-    dispatch({ type: "ADD" });
-  };
-  const sub = () => {
-    dispatch({ type: "SUB" });
-  };
   return (
     <div>
-      <button onClick={add}>+</button>
-      <button onClick={sub}>-</button>
+      <button onClick={() => dispatch(add())}>+</button>
+      <button onClick={() => dispatch(sub())}>-</button>
     </div>
   );
 };
---
+++ src/store/actions/action.js
@@ -0,0 +1,8 @@
+import { ADD, SUB } from "./actionTypes";
+
+export const add = () => ({
+  type: ADD,
+});
+export const sub = () => ({
+  type: SUB,
+});
