7da495816b50591b4001a663874b48260ddfe864
Bepul-Hossain <bepul.cse.ru@gmail.com>
2021-05-11 09:16:29 +0600

    useContext hook

--- src/App.js
+++ src/App.js
@@ -26,3 +26,6 @@ export default App;
 //যদি আমাদের একটা props, 1st Component থেকে 2nd Component,2nd Component থেকে 3rd Component (আরও অনেক) এ পাঠানো দরকার হয় ।
 //সেক্ষেত্রে আমরা সরাসরি যেকোনো component থেকে Data receive করতে পারি useContext Hook use করে ।
 //useContext Hook use করলে আমাদের নেস্টিং আকারে props পাঠানো লাগে না ।
+
+//useCOntext hook Provider অংশে কোন হেল্প করে না ।
+//useCOntext hook consumer অংশে হেল্প করে ।
--- src/componentD.js
+++ src/componentD.js
@@ -1,29 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
 import { NumberContext, ProfileContext } from "./App";
 
 const Display = () => {
   // Use the Consumer to grab the value from context
   // Notice this component didn't get any props!
+  const value = useContext(NumberContext);
+  const profile = useContext(ProfileContext);
   return (
-    <NumberContext.Consumer>
-      {(value) => {
-        return (
-          <div>
-            <ProfileContext.Consumer>
-              {(profile) => {
-                return (
-                  <div>
-                    The answer is {value} and {profile}
-                  </div>
-                );
-              }}
-            </ProfileContext.Consumer>
-            ;
-          </div>
-        );
-      }}
-    </NumberContext.Consumer>
+    <div>
+      The answer is {value} and {profile}
+    </div>
   );
 };
 
 export default Display;
+
+//as simple by using useContext hook
